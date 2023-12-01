96d2ddb8f7d6dc00effeeced63f66b65f8e0c3f5
Strohy <anujkumarsharma1808@gmail.com>
2023-12-01 14:06:18 +0530

    feat: added proof verification - unoptimized

--- verifier.py
+++ verifier.py
@@ -39,10 +39,14 @@ class VerificationKey:
     # efficiently batch them
     def verify_proof(self, group_order: int, pf, public=[]) -> bool:
         # 4. Compute challenges
-
+        beta, gamma, alpha, zeta, v, u = self.compute_challenges(pf)
+        self.beta, self.gamma = beta, gamma
+        
         # 5. Compute zero polynomial evaluation Z_H(ζ) = ζ^n - 1
+        Z_H_eval = zeta ** group_order - 1
 
         # 6. Compute Lagrange polynomial evaluation L_0(ζ)
+        
 
         # 7. Compute public input polynomial evaluation PI(ζ).
 
@@ -74,22 +78,123 @@ class VerificationKey:
     # Basic, easier-to-understand version of what's going on
     def verify_proof_unoptimized(self, group_order: int, pf, public=[]) -> bool:
         # 4. Compute challenges
+        beta, gamma, alpha, zeta, v, u = self.compute_challenges(pf)
+        self.beta, self.gamma = beta, gamma
+        pf = pf.flatten()
 
         # 5. Compute zero polynomial evaluation Z_H(ζ) = ζ^n - 1
+        Z_H_eval = zeta ** group_order - 1
 
         # 6. Compute Lagrange polynomial evaluation L_0(ζ)
+        L_0_eval = Z_H_eval / (group_order * (zeta - 1))
 
         # 7. Compute public input polynomial evaluation PI(ζ).
+        PI = Polynomial(
+            [Scalar(-v) for v in public]
+            + [Scalar(0) for _ in range(self.group_order - len(public))],
+            Basis.LAGRANGE,
+        )
+        # PI_eval = PI.barycentric_eval(zeta)
 
         # Recover the commitment to the linearization polynomial R,
         # exactly the same as what was created by the prover
+        zerotimesalpha = ec_lincomb([
+              (self.Qm, pf["a_eval"] * pf["b_eval"]),
+              (self.Ql, pf["a_eval"]),
+              (self.Qr, pf["b_eval"]),
+              (self.Qo, pf["c_eval"]),
+              (b.G1, PI.barycentric_eval(zeta)),
+              (self.Qc, Scalar(1)),
+        ])
+        
+        onetimesalpha = ec_lincomb([
+             (pf["z_1"],
+              self.rlc(pf["a_eval"], zeta)
+            * self.rlc(pf["b_eval"], zeta * 2)
+            * self.rlc(pf["c_eval"], zeta * 3)
+             ),
+
+             (self.S3,
+              -beta
+            * self.rlc(pf["a_eval"], pf["s1_eval"])
+            * self.rlc(pf["b_eval"], pf["s2_eval"])
+            * pf["z_shifted_eval"]
+             ),
+
+             (b.G1,
+              -(pf["c_eval"] + gamma)
+            * self.rlc(pf["a_eval"], pf["s1_eval"])
+            * self.rlc(pf["b_eval"], pf["s2_eval"])
+            * pf["z_shifted_eval"]
+              )
+        ])
+
+        twotimesalpha = ec_lincomb([
+             (pf["z_1"], L_0_eval),
+             (b.G1, -L_0_eval)
+        ])
+
+        T_sum = ec_lincomb([
+             (pf["t_lo_1"], 1),
+             (pf["t_mid_1"], zeta ** self.group_order),
+             (pf["t_hi_1"], zeta ** (self.group_order * 2))
+        ])
+
+        r1 = ec_lincomb([
+             (zerotimesalpha, 1),
+             (onetimesalpha, alpha),
+             (twotimesalpha, alpha**2),
+             (T_sum, -Z_H_eval)
+        ])
 
         # Verify that R(z) = 0 and the prover-provided evaluations
         # A(z), B(z), C(z), S1(z), S2(z) are all correct
+        lhs = b.pairing(
+             ec_lincomb([
+                  (self.X_2, 1),
+                  (b.G2, -zeta)
+             ]),
+             pf["W_z_1"]
+        )
+
+        rhs = b.pairing(b.G2, 
+            ec_lincomb(
+            [
+                (r1, 1),
+                (pf["a_1"], v),
+                (b.G1, -pf["a_eval"] * v),
+                (pf["b_1"], v**2),
+                (b.G1, -pf["b_eval"] * v**2),
+                (pf["c_1"], v**3),
+                (b.G1, -pf["c_eval"] * v**3),
+                (self.S1, v**4),
+                (b.G1, -pf["s1_eval"] * v**4),
+                (self.S2, v**5),
+                (b.G1, -pf["s2_eval"] * v**5),
+            ]
+            ))
+        
+        assert lhs == rhs
 
         # Verify that the provided value of Z(zeta*w) is correct
+        lhs = b.pairing(
+             ec_lincomb([
+                  (self.X_2, 1),
+                  (b.G2, -zeta * Scalar.root_of_unity(group_order))
+             ]),
+             pf["W_zw_1"]
+        )
 
-        return False
+        rhs = b.pairing(b.G2,
+             ec_lincomb([
+                  (pf["z_1"], 1),
+                  (b.G1, -pf["z_shifted_eval"])
+             ])
+        )
+
+        assert lhs == rhs
+        
+        return True
 
     # Compute challenges (should be same as those computed by prover)
     def compute_challenges(
@@ -103,3 +208,6 @@ class VerificationKey:
         u = transcript.round_5(proof.msg_5)
 
         return beta, gamma, alpha, zeta, v, u
+
+    def rlc(self, term_1, term_2):
+            return term_1 + term_2 * self.beta + self.gamma
